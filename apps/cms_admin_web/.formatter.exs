7040bb3a06afb778f84718699b914cdc89110dab
Leandro Pereira <leandro@leandro.io>
2025-02-06 12:08:24 -0500

    install :beacon_live_admin

--- apps/cms_admin_web/.formatter.exs
+++ apps/cms_admin_web/.formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:phoenix],
+  import_deps: [:beacon_live_admin, :phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
--- apps/cms_admin_web/lib/cms_admin_web/router.ex
+++ apps/cms_admin_web/lib/cms_admin_web/router.ex
@@ -1,6 +1,12 @@
 defmodule CmsAdminWeb.Router do
   use CmsAdminWeb, :router
 
+  use Beacon.LiveAdmin.Router
+
+  pipeline :beacon_admin do
+    plug Beacon.LiveAdmin.Plug
+  end
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +14,11 @@ defmodule CmsAdminWeb.Router do
     plug :put_root_layout, html: {CmsAdminWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+  end
+
+  scope "/" do
+    pipe_through [:browser, :beacon_admin]
+    beacon_live_admin "/admin"
   end
 
   scope "/", CmsAdminWeb do
